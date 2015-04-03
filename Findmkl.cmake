286dc95e01c2206e9f4c240f5f322a14d2fc2418
boruoshihao <boruoshihao@gmail.com>
2015-04-03 17:59:51 -0400

    Update findmkl.cmake, use int32 instead of int64.--prepare of magma.

--- Findmkl.cmake
+++ Findmkl.cmake
@@ -1,6 +1,6 @@
 #Try to find mkl
 set(mkl_path "$ENV{MKL}")
-find_library(lp64_libraries NAMES mkl_intel_ilp64 PATHS "${mkl_path}/lib/intel64")
+find_library(lp64_libraries NAMES mkl_intel_lp64 PATHS "${mkl_path}/lib/intel64")
 find_library(core_libraries NAMES mkl_core PATHS "${mkl_path}/lib/intel64")
 find_library(gnu_thread_libraries NAMES mkl_gnu_thread PATHS "${mkl_path}/lib/intel64")
 set(mkl_libraries ${lp64_libraries} ${core_libraries} ${gnu_thread_libraries})
